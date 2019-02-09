8a256f5f8d493c8f646f012c6e71af2f994c5132
RKIV <robertkellerIV@gmail.com>
2019-02-08 20:21:14 -0500

    Creates command to lower the elevator

---
+++ src/main/java/frc/robot/commands/DescendElevator.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class DescendElevator extends Command {
+  public DescendElevator() {
+    requires(Robot.frontElevator);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Elevator.elevatorSpark2.follow(Elevator.elevatorSpark1);
+    Elevator.elevatorSpark1.set(-0.5);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Elevator.elevatorBotSwitch.get();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Elevator.elevatorSpark1.stopMotor();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    Elevator.elevatorSpark1.stopMotor();
+  }
+}
